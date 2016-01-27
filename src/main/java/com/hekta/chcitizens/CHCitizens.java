d172124e8a98c38410910335ce6b67b599299036
PseudoKnight <michael.jt.smith@gmail.com>
2016-01-26 16:48:13 -0800

    Bump version to 1.1.5

--- src/main/java/com/hekta/chcitizens/CHCitizens.java
+++ src/main/java/com/hekta/chcitizens/CHCitizens.java
@@ -16,7 +16,7 @@ import com.laytonsmith.core.extensions.MSExtension;
 @MSExtension("CHCitizens")
 public class CHCitizens extends AbstractExtension {
 
-	private static final Version VERSION = new SimpleVersion(1, 1, 4);
+	private static final Version VERSION = new SimpleVersion(1, 1, 5);
 
 	private static MCCitizensPlugin _citizens;
 	private static MCCitizensNPCRegistry _registry;
